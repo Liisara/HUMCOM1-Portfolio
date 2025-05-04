1e30bf1746de07104392c76a395c799a2b12d9c4
Liisara <nocaraegshaeyruz@gmail.com>
2025-05-04 15:08:58 +0800

    Delete assets directory

--- assets/js/LabExer3.js
+++
@@ -1,6 +0,0 @@
-function getResult() {
-    var result = document.getElementById("number").value;
-    var product = result * 100000;
-    document.getElementById("result1").innerHTML = result + " KILOMETER IS " + product +" cm.";
-}
-    
