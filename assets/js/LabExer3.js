d527df7831f977cdec8c988e261009c8f2c968e1
Liisara <nocaraegshaeyruz@gmail.com>
2025-05-04 15:11:02 +0800

    Add files via upload

---
+++ assets/js/LabExer3.js
@@ -0,0 +1,6 @@
+function getResult() {
+    var result = document.getElementById("number").value;
+    var product = result * 100000;
+    document.getElementById("result1").innerHTML = result + " KILOMETER IS " + product +" cm.";
+}
+    
